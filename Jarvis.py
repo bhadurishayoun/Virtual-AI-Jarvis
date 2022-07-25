5fdaee71e5907276fb69f10ce2a0d6e0b306e058
bhadurishayoun <81213695+bhadurishayoun@users.noreply.github.com>
2022-07-26 00:53:43 +0530

    Add files via upload

---
+++ Jarvis.py
@@ -0,0 +1,11 @@
+from win32com.client  import Dispatch
+import win32
+import pyttsx3
+def speak():
+    engine.say(audio)
+    engine.runAndWait()
+
+
+
+if __name__=='__main__':
+    speak("I am Vengence")
